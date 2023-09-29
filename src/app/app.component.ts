6f47279a818c62b2d044259d0994770c2433d227
fvasconcellos <fernandovasconcellos67@gmail.com>
2023-09-28 20:38:51 -0500

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'requisicao-api-externa';
+
+  public pokemons$: Observable<any>;
+
+  constructor(private http: HttpClient) {
+    this.pokemons$ = this.http.get(
+      'https://pokeapi.co/api/v2/pokemon?limit=100&offset=200'
+    );
+  }
 }
